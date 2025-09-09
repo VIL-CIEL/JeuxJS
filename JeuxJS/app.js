c5725b446f834a061347bd7d76d17e179aecb324
Fabien Villedieu <fabien.villedieu.pro@gmail.com>
2025-09-09 09:38:30 +0200

    #3 WebSocket

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -1,11 +1,10 @@
-'use strict';
+﻿'use strict';
 
 /*  *********************** Serveur Web ***************************   */
 //
-var port = 80;
-
 var express = require('express');
 var exp = express();
+exp.use(express.static(__dirname + '/www')); 
 
 exp.get('/', function (req, res) {
     console.log('Reponse a un client'); 
@@ -17,6 +16,32 @@ exp.use(function (err, req, res, next) {
     res.status(500).send('Erreur serveur express');
 }); 
 
-exp.listen(port, function () {
-    console.log('Serveur en ecoute');
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
 }); 
+
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
+    console.log('Serveur en ecoute');
+});
