df5db7941c693e5f9387d56f830f321d9ff8dd62
Fabien Villedieu <fabien.villedieu.pro@gmail.com>
2025-09-09 09:12:27 +0200

    #2 Serveur Web

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Hello world');
+/*  *********************** Serveur Web ***************************   */
+//
+var port = 80;
+
+var express = require('express');
+var exp = express();
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+}); 
